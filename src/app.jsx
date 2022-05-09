90946d173383363c05bcdb6c47a0abdc2ba4f919
ksubin98 <subin.k1229@gmail.com>
2022-05-09 14:27:57 +0900

    initial set

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,7 @@ import styles from './app.module.css';
 function App({authService}) {
   return (
     <div className={styles.app}>
-      <Login authService={authService}/>
+      <Login/>
     </div>
   )
 }
